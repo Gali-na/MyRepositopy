29a2f192a63b1c2ff8fbabe4922e9b7c4c83ccac
Tsivka Galina <galinatsivka@gmail.come>
2020-08-05 23:49:23 +0300

    two

--- src/Pepository.java
+++ src/Pepository.java
@@ -2,5 +2,6 @@ public class Pepository {
     public static void main (String args[]){
 System.out.println("Hallo");
         System.out.println("Hallo");
+        System.out.println("Hallo");
     }
 }
